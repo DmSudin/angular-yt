9b92f30ad34ba4a5151624cbf76ee9ebb58a239e
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-12 21:53:16 +0300

    my first commit

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.scss']
+})
+export class CarComponent {
+  carName = 'Ford';
+  carYear = 2017;
+
+  getName() {
+    return this.carName;
+  }
+}
