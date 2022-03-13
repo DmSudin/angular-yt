1735713e810c1d3f26abc2f01ddb2175143c7995
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-13 21:00:59 +0300

    lesson 16

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-cars',
     templateUrl: './cars.component.html',
-    styleUrls: ['./cars.component.scss']
+    styleUrls: ['./cars.component.scss'],
 })
 export class CarsComponent {
 
