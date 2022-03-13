0bb3aa258f9af3e308685f85603d7b1a6af891e0
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-13 22:40:29 +0300

    lesson 18

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -8,4 +8,9 @@ import { Component, Input } from '@angular/core';
 export class CarComponent {
 
   @Input() car: {name: string, year: number};
+  @ContentChild('carHeading') carHeading: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.carHeading);
+  }
 }
