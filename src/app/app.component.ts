477365cf9bfc93577ad5a4720667998d7cc0d785
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-17 12:18:33 +0300

    lesson 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  headerText = 'Creating of your own directive';
+    headerText = 'Creating of your own directive';
+    items = [1, 2, 3, 4, 5];
+    current = 1;
+
+    onClick(number: number) {
+        this.current = number;
+    }
 }
