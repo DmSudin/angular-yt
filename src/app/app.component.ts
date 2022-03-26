b6cc59422e5d953a4bac23ef0a6ebb44b76908ef
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-26 10:14:29 +0300

    lesson 39 - forms 7 - dynamic changes in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 
@@ -8,14 +8,35 @@ import { NgForm } from '@angular/forms';
 
 })
 export class AppComponent {
+    @ViewChild('form') form: NgForm;
     answers = [
         {type: 'yes', text: 'Da'},
         {type: 'no', text: 'Net'},
     ];
     defaultAnswer = 'no';
     defaultCountry = 'ru';
+    formData = {};
+    isSubmitted = false;
 
-    submitForm(form: NgForm) {
-        console.error('Submitted!', form);
+    addRandEmail() {
+        const randEmail = 'wfm@gmail.com';
+        // this.form.setValue({
+        //     user: {
+        //         pass: '',
+        //         email: randEmail
+        //     },
+        //     country: '',
+        //     answer: '',
+        // });
+        this.form.form.patchValue({
+            user: {email: randEmail}
+        })
+    }
+
+    submitForm() {
+        this.isSubmitted = true;
+        this.formData = this.form.value;
+        this.form.reset();
+        console.error('Submitted!', this.form);
     }
 }
