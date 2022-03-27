e14144898095a25ee659f80de7b4acb5a110fcd3
Dmitriy Sudin <dmitriy.sudin@softgamings.com>
2022-03-27 10:56:27 +0300

    lesson 47 - web server - output data to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarsService } from './cars.service';
+import { HttpResponse } from '@angular/common/http';
 
+interface ICar {
+    name: string,
+    color: string,
+    id: number
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-
 })
 export class AppComponent {;
-   cars = [];
+   cars : ICar[];
 
    constructor(private carsService: CarsService) {
 
    }
 
    loadCars() {
-       this.carsService
+       
+ // NOT very correct approach
+    //    this.carsService
+    //    .getCars()
+    //    .subscribe((response: Array<any>) => {
+    //        this.cars = response;
+    //    });
+
+// THIS ONE IS GOOD
+           this.carsService
        .getCars()
-       .subscribe((response) => {
-           console.error(response);
+       .subscribe((cars: ICar[]) => {
+           this.cars = cars
        });
    }
 }
